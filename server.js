9f05f936f1df488fbc741a379a20327e376453af
Ankit <ankit31894@gmail.com>
2016-06-09 16:39:46 +0530

    initial files

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 var express = require("express");
-var http = require("http");
-var checkUrl=require("./validUrl");
+var http = require("https");
 var app = express();
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
@@ -12,63 +11,38 @@ var MongoClient = mongodb.MongoClient;
 
 //(Focus on This Variable)
 
-var json_res={};
 app.get("/", function(request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.write("Use /new/url to get the tinyurl of a site\n");
     response.end("Or enter  the tinyurl of a site");
 });
-app.get("/new/*", function(request, response,next) {
-      response.writeHead(200, { "Content-Type": "text/plain" });
-
-    var url=request.params[0];
-    if(checkUrl(url)){
-        json_res={
-            "url":url,
-        }
-
-        var mongo_url = process.env.MONGOLAB_URI;      
-        //(Focus on This Variable)
-
-        // Use connect method to connect to the Server
-        MongoClient.connect(mongo_url, function (err, db) {
-            if (err) {
-                console.log('Unable to connect to the mongoDB server. Error:', err);
-            }
-            else {
-                console.log('Connection established to', mongo_url);
-                var collection = db.collection('urls')
-                collection.find().limit(1).sort({$natural:-1}).toArray(function(err,doc){
-                    if(doc.length===0)
-                        json_res.id=0;
-                    else 
-                        json_res.id=doc[0].id+1;
-                     collection.insert(json_res, function(err,s) {
-                         json_res.tiny_url=(request.protocol + '://' + request.get('host') + request.url).replace("new/"+request.params[0],"")+json_res.id;
-                        var str=JSON.stringify(json_res);
-                        response.end(str);
-                        db.close();
-                     });
-                });
-            }
+app.get("/api/imagesearch/:string", function(request, response,next) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    console.log("F");
+    var q=request.params.string;
+    var num=request.query.offset||10;
+    getM("https://www.googleapis.com/customsearch/v1?q="+q+"&num="+num+"&searchtype=image&cx=015740443609325608978%3Achvejfx7lbq&key=AIzaSyD0jyiko1BDpDYWqtGGNw-rwiNYMN6T9Yw",function(out){
+        response.end(output);
+    });
+    function getM(options,callback) {
+        http.get(options, function(response) {
+            // Continuously update stream with data
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+                callback(body);
+            });
         });
     }
-    else{
-         json_res={
-            "url":"wrong url"
-         }
-        var str=JSON.stringify(json_res);
-        response.end(str);
 
-    }
-    
 });
 app.get("/:number", function(request, response,next) {
-
     
+    console.log("G");
     var mongo_url = process.env.MONGOLAB_URI;      
     //(Focus on This Variable)
-
     // Use connect method to connect to the Server
     MongoClient.connect(mongo_url, function (err, db) {
         if (err) {
