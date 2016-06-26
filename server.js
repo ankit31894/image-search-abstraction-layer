508dd14de0469765bbc1fdc59e714fc1beb26d84
Ankit <ankit31894@gmail.com>
2016-06-26 21:12:51 +0530

    Fixed offset

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 var express = require("express");
-var http = require("https");
+var https = require("https");
+var fs = require('fs');
 var app = express();
-
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
+var mongo_url = process.env.MONGOLAB_URI||'mongodb://localhost:27017/my_database_name';
 
 // Connection URL. This is where your mongodb server is running.
 
@@ -13,19 +18,44 @@ var MongoClient = mongodb.MongoClient;
 
 app.get("/", function(request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Use /new/url to get the tinyurl of a site\n");
-    response.end("Or enter  the tinyurl of a site");
+    response.write("Use /api/imagesearch/any_value to search the image\n");
+    response.end("Or /api/latest/imagesearch/ to get latest searches");
 });
 app.get("/api/imagesearch/:string", function(request, response,next) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    console.log("F");
     var q=request.params.string;
-    var num=request.query.offset||10;
-    getM("https://www.googleapis.com/customsearch/v1?q="+q+"&num="+num+"&searchtype=image&cx=015740443609325608978%3Achvejfx7lbq&key=AIzaSyD0jyiko1BDpDYWqtGGNw-rwiNYMN6T9Yw",function(out){
-        response.end(output);
+    var num=Math.min(request.query.offset||0,90);
+    var ar=[];
+    getM("https://www.googleapis.com/customsearch/v1?q="+q+"&start="+num+"&searchtype=image&cx=015740443609325608978%3Achvejfx7lbq&key=AIzaSyD0jyiko1BDpDYWqtGGNw-rwiNYMN6T9Yw",function(out){
+        var json_res=JSON.parse(out);
+        for(var i=0;i<json_res.items.length;i++){
+            if(json_res.items[i].pagemap.cse_image!=undefined)
+            ar.push({
+                alt:json_res.items[i].snippet,
+                url:json_res.items[i].pagemap.cse_image[0].src,
+                page_url:json_res.items[i].link
+            });
+        }
+
+        // Use connect method to connect to the Server
+        MongoClient.connect(mongo_url, function (err, db) {
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            }
+            else {
+                console.log('Connection established to', mongo_url);
+                var collection = db.collection('ial')
+                 collection.insert({string:q}, function(err,s) {
+                     console.log(s);
+                    db.close();
+                 });
+            }
+        });
+
+        response.end(JSON.stringify(ar));
     });
     function getM(options,callback) {
-        http.get(options, function(response) {
+        https.get(options, function(response) {
             // Continuously update stream with data
             var body = '';
             response.on('data', function(d) {
@@ -38,11 +68,8 @@ app.get("/api/imagesearch/:string", function(request, response,next) {
     }
 
 });
-app.get("/:number", function(request, response,next) {
-    
-    console.log("G");
-    var mongo_url = process.env.MONGOLAB_URI;      
-    //(Focus on This Variable)
+app.get("/api/latest/imagesearch", function(request, response,next) {
+
     // Use connect method to connect to the Server
     MongoClient.connect(mongo_url, function (err, db) {
         if (err) {
@@ -50,27 +77,24 @@ app.get("/:number", function(request, response,next) {
         }
         else {
             console.log('Connection established to', mongo_url);
-            var collection = db.collection('urls')
-            collection.find({"id":parseInt(request.params.number)}).limit(1).toArray(function(err,doc){
-                if(doc.length===0){
-                    json_res={"result":"no url to redirect"};
-                    response.end(JSON.stringify(json_res));
+            var collection = db.collection('ial')
+            collection.find().limit(10).sort({_id: -1}).toArray(function(err,doc){
+                var temp=[];
+                for(var i=0;i<doc.length;i++){
+                    temp.push({string:doc[i].string,time:doc[i]._id.getTimestamp()});
                 }
-                else {
-                    response.setHeader("Location", doc[0].url);
-                    response.writeHead(302);
-                    response.end();
-                }
+                response.end(JSON.stringify(temp));
                 db.close();
             });
         }
-    });    
-    
+    });
+
 });
 app.get("*", function(request, response) {
     response.end("404!");
 });
-http.createServer(app);
+https.createServer(options, app);
+//http.createServer(app);
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
